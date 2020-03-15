a7b0de098f50bf30e8f3e9c6b71c7cd78cf7bdd3
Noppanut-Wapee <machanics.02@gmail.com>
2020-03-15 18:38:43 +0700

    ทบทวน SET

---
+++ Basic Python/Data Type3.py
@@ -0,0 +1,20 @@
+# Set ใช้ {}
+# set ไม่มีลำดับที่แน่นอน ระบุ Pointer ไม่ได้
+# set แก้ไขข้อมูลได้เหมือน List 
+
+a = {1,2,4,5,6,}
+print(a)
+
+a.add(12)
+a.add(123)
+print(a)
+
+for x in a:
+    print(x)
+
+#set.union
+#set.intersection
+#set.difference
+#set.symmetric_differrence
+
+
