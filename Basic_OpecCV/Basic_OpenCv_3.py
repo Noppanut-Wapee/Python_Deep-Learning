11ce07e163c3bf76131e328e0a21ce7922f253c5
Noppanut-Wapee <machanics.02@gmail.com>
2020-03-19 23:35:36 +0700

    a

---
+++ Basic_OpecCV/Basic_OpenCv_3.py
@@ -0,0 +1,17 @@
+import cv2 
+import numpy as np  
+from matplotlib import pyplot as plt  
+
+img = cv2.imread('/Users/noppanutwapee/Desktop/Python_Deep-Learning/Project_pimc+/Pic Test/meter.jpg',cv2.IMREAD_COLOR)
+
+plt.imshow(img, cmap='gray',interpolation='bicubic')
+plt.xticks([]),plt.yticks([])
+plt.show
+#cv2.waitKey(0)
+
+#cv2.imwrite('pun.png',img1)
+
+
+#cv2.destroyAllWindows
+
+
