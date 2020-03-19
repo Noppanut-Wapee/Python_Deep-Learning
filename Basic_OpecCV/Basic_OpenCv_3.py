377875098108bc2df82059f4d43c5094dd43c280
Noppanut-Wapee <machanics.02@gmail.com>
2020-03-19 23:40:20 +0700

    matplotlib

--- Basic_OpecCV/Basic_OpenCv_3.py
+++ Basic_OpecCV/Basic_OpenCv_3.py
@@ -5,8 +5,8 @@ from matplotlib import pyplot as plt
 img = cv2.imread('/Users/noppanutwapee/Desktop/Python_Deep-Learning/Project_pimc+/Pic Test/meter.jpg',cv2.IMREAD_COLOR)
 
 plt.imshow(img, cmap='gray',interpolation='bicubic')
-plt.xticks([]),plt.yticks([])
-plt.show
+plt.xticks([1]),plt.yticks([1])
+#plt.show()
 #cv2.waitKey(0)
 
 #cv2.imwrite('pun.png',img1)
