bcfacc96b62a528f7d9bf29f6db478040a95dbca
Noppanut-Wapee <machanics.02@gmail.com>
2020-03-25 19:36:14 +0700

    Master3

--- Project_pimc+/Master_3.py
+++ Project_pimc+/Master_3.py
@@ -1,11 +1,15 @@
-# import the necessary packages
-from imutils.perspective import four_point_transform
-# library imutils ใช้เกี่ยวกัว ปรับภาพ รีขนาดภาพ
-from imutils import contours
-import imutils
+from scipy.spatial import distance as dist
+import numpy as np
 import cv2
+# import the necessary packages
+#from imutils.perspective import four_point_transform
+# library imutils ใช้เกี่ยวกัว ปรับภาพ รีขนาดภาพ
+#from imutils import contours
+import imutils
+#import cv2
 # define the dictionary of digit segments so we can identify
 # each digit on the thermostat
+
 
 DIGITS_LOOKUP = {
 	(1, 1, 1, 0, 1, 1, 1): 0,
@@ -21,19 +25,20 @@ DIGITS_LOOKUP = {
 }
 
 # load the example image
-image = cv2.imread('/Users/noppanutwapee/Desktop/Python_Deep-Learning/Project_pimc+/Pic Test/t1.jpg')
+image = cv2.imread('/Users/noppanutwapee/Desktop/Python_Deep-Learning/Project_pimc+/Pic Test/test4.jpg')
 # pre-process the image by resizing it, converting it to
 # graycale, blurring it, and computing an edge map
 # library imutils ใช้เกี่ยวกัว ปรับภาพ รีขนาดภาพ
 # ปรับรูป ขนาด 500 pixle
-image = imutils.resize(image, height=500) 
+image = imutils.resize(image,height=500) 
 # การวิเคราะห์รูปจะต้องแปลงให้เป็น gray scale 0-255
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
 # Gaussian การใส่ noise เข้าไปในภาพ จะทำให้ง่ายต่อการกรองภาพ Gaussian filter kernel 
 blurred = cv2.GaussianBlur(gray, (5, 5), 0) 
 # Canny edge detector เป็นวิธีตรวจจับความกว้างในขอบภาพ
-edged = cv2.Canny(blurred, 50, 200, 255)
+edged = cv2.Canny(blurred, 50, 50, 50)
 '''
+
 cv2.imshow('Original pic',image)
 cv2.waitKey(0)
 cv2.imshow('gray',gray)
@@ -67,14 +72,82 @@ for c in cnts:
 # extract the thermostat display, apply a perspective transform
 # to it
 
+def order_points(pts):
+    # sort the points based on their x-coordinates
+    xSorted = pts[np.argsort(pts[:, 0]), :]
+
+    # grab the left-most and right-most points from the sorted
+    # x-roodinate points
+    leftMost = xSorted[:2, :]
+    rightMost = xSorted[2:, :]
+
+    # now, sort the left-most coordinates according to their
+    # y-coordinates so we can grab the top-left and bottom-left
+    # points, respectively
+    leftMost = leftMost[np.argsort(leftMost[:, 1]), :]
+    (tl, bl) = leftMost
+
+    # now that we have the top-left coordinate, use it as an
+    # anchor to calculate the Euclidean distance between the
+    # top-left and right-most points; by the Pythagorean
+    # theorem, the point with the largest distance will be
+    # our bottom-right point
+    D = dist.cdist(tl[np.newaxis], rightMost, "euclidean")[0]
+    (br, tr) = rightMost[np.argsort(D)[::-1], :]
+
+    # return the coordinates in top-left, top-right,
+    # bottom-right, and bottom-left order
+    return np.array([tl, tr, br, bl], dtype="float32")
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    # return the warped image
+    return warped
 
 warped = four_point_transform(gray, displayCnt.reshape(4, 2))
 output = four_point_transform(image, displayCnt.reshape(4, 2))
+
+
+
 
 cv2.imshow('Original',image)
 cv2.imshow('test',warped)
 cv2.imshow('test1',output)
 cv2.waitKey(0)
+
 
 '''
 # threshold the warped image, then apply a series of morphological
