9cef6d2c821fa3c94ce452eb5ff26f6ca469663c
Noppanut-Wapee <machanics.02@gmail.com>
2020-03-30 23:38:38 +0700

    edabit challenge

---
+++ Edabit Challenge/Fullname and Email .py
@@ -0,0 +1,11 @@
+'''
+emp_1 = Employee("John", "Smith")
+emp_2 = Employee("Mary",  "Sue")
+emp_3 = Employee("Antony", "Walker")
+
+emp_1.fullname ➞ "John Smith"
+emp_2.email ➞ "mary.sue@company.com"
+emp_3.firstname ➞ "Antony"
+
+'''
+
