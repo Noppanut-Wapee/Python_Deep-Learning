8b9b3712bca31cc0acb866c208aae1a468b4936a
Noppanut-Wapee <machanics.02@gmail.com>
2020-04-02 10:34:25 +0700

    hackerrank

--- Edabit Challenge/Hackerrank.py
+++ Edabit Challenge/Hackerrank.py
@@ -1,9 +1,17 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    ar = []
+    p = 0
+    for i in range ( x + 1 ) :
+         for j in range( y + 1):
+                 for k in range(z+1):
+                         if i+j+k != n:
+                                ar.append([])
+                                ar[p] = [ i , j, k ]
+                                p+=1        
+    print(ar)
 
-a = 4
-b = 3
-n = 10
-num = ''
-for i in range(1,n+1):
-        i = str(i)
-        num = num + i
-print(num)
+    
